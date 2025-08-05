3b5a2cc919256e0f0ffffc0bb0d399c2853fab5b
Pri-Destro <priyanshs253@gmail.com>
2025-08-05 17:26:25 +0530

    basic backend done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,51 +1,66 @@
 import express from "express"
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 import { middleware } from "./middleware";
 import {JWT_SECRET}  from "@repo/backend-common/config"; 
 import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types";
 import {prismaClient} from "@repo/db/client"
 
 const app = express();
+app.use(express.json())
 
-app.post("/signup",(req,res) => {
+app.post("/signup", async (req,res) => {
     
     const parsedData = CreateUserSchema.safeParse(req.body);
 
     if(!parsedData.success){
-        return res.status(400).json({
+        res.status(400).json({
             error : "Invalid Inputs"
+        })
+        return
+    }
+
+    const hashedPassword = bcrypt.hash(parsedData.data.password, 10)
+
+    try{
+        const user = await prismaClient.user.create({
+            data : {
+                userId : parsedData.data.username,
+                password : hashedPassword,
+                name : parsedData.data.name
+            }
+        })
+        
+        if(!user){
+            res.json({
+                message : "Not authorized"
+            })
+        }
+
+        res.json({
+            userId : user.id,
+        })
+    }catch(e){
+        res.status(403).json({
+            message : "Error at db"
         })
     }
 
-    prismaClient.user.create({
-        data : {
-            userId : parsedData.data.username,
-            password : parsedData.data.password,
-            name : parsedData.data.name
-        }
-    })
-    
-    
-    res.json({
-        userId : "12345",
-    })
-
 })
 
-app.post("/singin",(req,res) => {
+app.post("/signin",(req,res) => {
 
-    const data = SigninSchema.safeParse(req.body);
+    const parsedData = SigninSchema.safeParse(req.body);
 
-    if(!data.success){
+    if(!parsedData.success){
         return res.status(400).json({
             error : "Invalid data"
         })
     }
 
-    const userId = 1;
+    const userId = parsedData.data.username;
     const token = jwt.sign({
-        userId},
-        JWT_SECRET);
+        userId},JWT_SECRET);
 
     res.json({
         token
@@ -53,20 +68,72 @@ app.post("/singin",(req,res) => {
 
 })
 
-app.get("/room",middleware, (req,res)=>{
+app.get("/room",middleware, async (req,res)=>{
 
-    const data = CreateRoomSchema.safeParse(req.body);
+    const parsedData = CreateRoomSchema.safeParse(req.body);
 
-    if(!data.success){
-        return res.status(400).json({
+    if(!parsedData.success){
+        res.status(400).json({
             error : "Invalid data"
+        })
+        return;
+    }
+
+    //@ts-ignore
+    const userId = req.userId //from middleware
+
+    try{
+
+        const room = await prismaClient.user.create({
+            data : {
+                slug : parsedData.data.roomName,
+                adminId : userId
+            }
+        })
+
+        res.json({
+            roomId : room.id,
+        })
+
+    }catch(e){
+        res.status(403).json({
+            message : "Error at db"
         })
     }
     
-    res.json({
-        roomId : "room123"
+})
+
+app.get("/chat/:roomId", async (req,res)=>{
+    const roomId = Number(req.params.roomId);
+
+    const messages = await prismaClient.chat.findMany({
+        where : {
+            roomId : roomId
+        },
+        order : {
+            id : "desc"
+        },
+        take : 50
+
     })
 
+    res.json({
+        messages
+    })
+})
+
+app.get("/room/:slug", async (req,res)=>{
+    const slug = req.params.slug;
+
+    const room = await prismaClient.room.findFirst({
+        where : {
+            slug 
+        }
+    })
+
+    res.json({
+        room
+    })
 })
 
 app.listen(3000);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,35 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer,WebSocket } from 'ws';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { JWT_SECRET }  from "@repo/backend-common/config";
+import {prismaClient} from "@repo/db/client";
 
 const wss = new WebSocketServer({ port: 8080 });
+
+
+interface Users{
+  ws : WebSocket,
+  rooms : String[],
+  userId : String
+}
+
+const users : Users[] = [];
+
+function checkUser(token : string) : string | null{
+  try{
+      const decoded = jwt.verify(token,JWT_SECRET)
+
+      if(!decoded || !(decoded as JwtPayload).userId){
+        wss.close()
+        return null
+      }
+    
+       return (decoded as JwtPayload).userId
+
+  }catch(e){
+    return null
+  }
+
+}
 
 wss.on('connection', function connection(ws,request) {
 
@@ -12,17 +39,60 @@ wss.on('connection', function connection(ws,request) {
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || " ";
 
-  const decoded = jwt.verify(token, JWT_SECRET);
+  const userId = checkUser(token);
 
-  if(!decoded || !(decoded as JwtPayload).userId){
-    ws.close();
-    return;
+  if(userId == null) {
+    ws.close()
+    return null
   }
 
-  ws.on('error', console.error);
+  users.push({    // on every connection each userId/user gets pushed to users 
+    userId,
+    rooms : [],
+    ws 
+  })
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on('message', async function message(data) {
+
+    const parsedData = JSON.parse(data as unknown as string)  // data will be of {type : "join room", roomId : room1}
+    
+    if(parsedData.type === "join_room"){
+      const user = users.find(x => x.ws === ws)
+      user?.rooms.push(parsedData.roomId)
+    }
+
+    if(parsedData.type == "leave_room"){
+      const user = users.find(x => x.ws === ws)
+
+      if(!user) return;
+
+      user.rooms = user?.rooms.filter(x => x !== parsedData.room)
+    }
+
+    if(parsedData.type == "chat"){            // data will be like {type : chat, message : hi, roomId : 123}
+      const roomId = parsedData.roomId;
+      const message = parsedData.message;
+
+      await prismaClient.chat.create({        // (temp approach) messages will first store in db then it will broadcast
+        data : {
+          roomId,
+          message,
+          userId
+          
+        }
+      })
+
+      users.forEach(user => {
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type : "chat",
+            message : message,
+            roomId
+          }))
+        }
+      })
+    }
+    
   });
 
   ws.send('something');
